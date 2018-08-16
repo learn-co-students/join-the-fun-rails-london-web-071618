eccd5672cad2967b6a1da6689ac1ea96db4a314d
kreyes12 <karla.y.reyes12@gmail.com>
2018-08-16 13:29:01 +0100

    Done.

--- db/migrate/20140212205242_create_rides.rb
+++ db/migrate/20140212205242_create_rides.rb
@@ -2,6 +2,8 @@ class CreateRides < ActiveRecord::Migration
   def change
     create_table :rides do |t|
       t.timestamps null: false
+      t.belongs_to :passenger, foreign_key: true
+      t.belongs_to :taxi, foreign_key: true
     end
   end
 end
